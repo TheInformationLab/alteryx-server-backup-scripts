fc7b27b2a1002be1b31e82793211212b7db56ca0
Ian Baldwin <ibaldwin1993@gmail.com>
2021-06-01 16:34:30 +0100

    move .dmp files with logs

--- batch-scripts/Alteryx-log-mover.bat
+++ batch-scripts/Alteryx-log-mover.bat
@@ -40,6 +40,7 @@ echo. >> %MoveLog%
 ::-----------------------------------------------------------------------------
 
 ROBOCOPY %GalleryLogDir% %TempDest% *.log /MOV /S /MINAGE:%LogAge% >> %MoveLog%
+ROBOCOPY %GalleryLogDir% %TempDest% *.dmp /MOV /S /MINAGE:%LogAge% >> %MoveLog%
 
 ::-----------------------------------------------------------------------------
 :: This section compresses the logs to a single zip archive
