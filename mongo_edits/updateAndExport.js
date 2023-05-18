38be9d35acda8b20f119cb9d4a25ec9c36bd0ead
Paul <Sivivatu@users.noreply.github.com>
2023-05-18 16:08:55 +0100

    Create updateAndExport.js
    adding mongodb schedule edit script for disabling all schedules

---
+++ mongo_edits/updateAndExport.js
@@ -0,0 +1,19 @@
+// Specify the database name and output file name
+const databaseName = 'your_database_name';
+const outputFile = 'documents.json';
+
+// Import the fs module
+const fs = require('fs');
+
+// Connect to the MongoDB instance
+const connection = new Mongo();
+const db = connection.getDB(databaseName);
+
+// Find all the documents where "enabled" is true
+const documentsToUpdate = db.AS_schedules.find({ enabled: true }).toArray();
+
+// Update the "enabled" value to false for all the retrieved documents
+db.AS_schedules.updateMany({ enabled: true }, { $set: { enabled: false } });
+
+// Write the documentsToUpdate array to a JSON file
+fs.writeFileSync(outputFile, JSON.stringify(documentsToUpdate, null, 2));
